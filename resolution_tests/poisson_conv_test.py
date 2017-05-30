4c753be1a56c5cbbe094fda661598c89e72a2e36
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-30 14:47:03 +0100

    Update params against current Firedrake for Poisson conv test

--- resolution_tests/poisson_conv_test.py
+++ resolution_tests/poisson_conv_test.py
@@ -25,14 +25,14 @@ def RT0DGO0(res, degree):
     L = f * v * dx
     w = Function(W)
 
-    solver_parameters = {'mat_type': 'matfree',
+    solver_parameters = {'ksp_type': 'preonly',
+                         'mat_type': 'matfree',
                          'pc_type': 'python',
                          'pc_python_type': 'firedrake.HybridizationPC',
-                         'trace_ksp_rtol': 1e-10,
-                         'trace_pc_type': 'lu',
-                         'trace_ksp_type': 'preonly',
-                         'ksp_monitor': True,
-                         'trace_ksp_monitor': True}
+                         'hybridization_pc_type': 'lu',
+                         'hybridization_ksp_type': 'preonly',
+                         'hybridization_pc_factor_mat_solver_package': 'mumps',
+                         'hybridization_projector_tolerance': 1e-14}
 
     solve(a == L, w, solver_parameters=solver_parameters)
     _, u_h = w.split()
@@ -61,14 +61,14 @@ def BDM1DGO0(res, degree):
     L = f * v * dx
     w = Function(W)
 
-    solver_parameters = {'mat_type': 'matfree',
+    solver_parameters = {'ksp_type': 'preonly',
+                         'mat_type': 'matfree',
                          'pc_type': 'python',
                          'pc_python_type': 'firedrake.HybridizationPC',
-                         'trace_ksp_rtol': 1e-10,
-                         'trace_pc_type': 'lu',
-                         'trace_ksp_type': 'preonly',
-                         'ksp_monitor': True,
-                         'trace_ksp_monitor': True}
+                         'hybridization_pc_type': 'lu',
+                         'hybridization_ksp_type': 'preonly',
+                         'hybridization_pc_factor_mat_solver_package': 'mumps',
+                         'hybridization_projector_tolerance': 1e-14}
 
     solve(a == L, w, solver_parameters=solver_parameters)
     _, u_h = w.split()
