3bb77ca175277c4345af416571a8843279d32058
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-30 14:45:39 +0100

    Update Poisson sphere demo

--- experiments/sphere_poisson.py
+++ experiments/sphere_poisson.py
@@ -2,36 +2,39 @@ from __future__ import absolute_import, print_function, division
 
 from firedrake import *
 
-mesh = UnitIcosahedralSphereMesh(refinement_level=5)
+mesh = UnitIcosahedralSphereMesh(refinement_level=3, degree=3)
 mesh.init_cell_orientations(Expression(("x[0]", "x[1]", "x[2]")))
+x, y, z = SpatialCoordinate(mesh)
 
-degree = 0
-RT_elt = FiniteElement("RT", triangle, degree + 1)
-V = FunctionSpace(mesh, RT_elt)
-U = FunctionSpace(mesh, "DG", degree)
-W = V * U
+V = FunctionSpace(mesh, "BDM", 2)
+U = FunctionSpace(mesh, "DG", 1)
+W = U * V
 
 f = Function(U)
-expr = Expression("x[0]*x[1]*x[2]")
-f.interpolate(expr)
+f.interpolate(x*y*z)
 
-sigma, u = TrialFunctions(W)
-tau, v = TestFunctions(W)
+u_exact = Function(U, name="exact scalar").interpolate(x*y*z/12.0)
 
-a = (dot(sigma, tau) - div(tau) * u + v * div(sigma)) * dx
-L = f * v * dx
+u, sigma = TrialFunctions(W)
+v, tau = TestFunctions(W)
+
+a = (dot(sigma, tau) - div(tau)*u + v*div(sigma))*dx
+L = f*v*dx
+
+params = {"ksp_type": "preonly",
+          "ksp_monitor": True,
+          "mat_type": "matfree",
+          "pc_type": "python",
+          "pc_python_type": "firedrake.HybridizationPC",
+          "hybridization_pc_type": "lu",
+          "hybridization_ksp_type": "preonly",
+          "hybridization_projector_tolerance": 1e-14}
+
+nullsp = MixedVectorSpaceBasis(W, [VectorSpaceBasis(constant=True), W[1]])
 w = Function(W)
+solve(a == L, w, nullspace=nullsp, solver_parameters=params)
 
-solver_parameters = {'mat_type': 'matfree',
-                     'pc_type': 'python',
-                     'pc_python_type': 'firedrake.HybridizationPC',
-                     'trace_ksp_rtol': 1e-13,
-                     'trace_pc_type': 'lu',
-                     'trace_ksp_type': 'preonly',
-                     'ksp_monitor': True}
+pdat, vdat = w.split()
 
-nullspace = MixedVectorSpaceBasis(W, [W[0], VectorSpaceBasis(constant=True)])
-solve(a == L, w, nullspace=nullspace, solver_parameters=solver_parameters)
-sigma_h, u_h = w.split()
-
-File("SpherePoisson-hybrid.pvd").write(sigma_h, u_h)
+print(errornorm(pdat, u_exact))
+File("sphere.pvd").write(vdat, pdat, u_exact)
