9948f53ea687de88e36c78c803df6a88196dabca
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-31 12:53:49 +0100

    Add an extruded experiment for trace coefficient testing

--- experiments/trace_coefficient_test.py
+++ experiments/trace_coefficient_test.py
@@ -4,40 +4,87 @@ from firedrake import *
 
 qflag = False
 degree = 1
+extrusion = True
 
-mesh = UnitSquareMesh(2, 2, quadrilateral=qflag)
-n = FacetNormal(mesh)
+if extrusion:
+    base = UnitSquareMesh(2, 2, quadrilateral=qflag)
+    mesh = ExtrudedMesh(base, layers=2, layer_height=0.5)
+    n = FacetNormal(mesh)
 
-if qflag:
-    RT = FiniteElement("RTCF", quadrilateral, degree)
-    DG = FiniteElement("DQ", quadrilateral, degree - 1)
-    Te = FiniteElement("HDiv Trace", quadrilateral, degree - 1)
+    if qflag:
+        RT = FiniteElement("RTCF", quadrilateral, 1)
+        DG_v = FiniteElement("DG", interval, 0)
+        DG_h = FiniteElement("DQ", quadrilateral, 0)
+        CG = FiniteElement("CG", interval, 1)
+
+    else:
+        RT = FiniteElement("RT", triangle, 1)
+        DG_v = FiniteElement("DG", interval, 0)
+        DG_h = FiniteElement("DG", triangle, 0)
+        CG = FiniteElement("CG", interval, 1)
+
+    HDiv_ele = EnrichedElement(HDiv(TensorProductElement(RT, DG_v)),
+                               HDiv(TensorProductElement(DG_h, CG)))
+    V = FunctionSpace(mesh, BrokenElement(HDiv_ele))
+    U = FunctionSpace(mesh, "DG", 0)
+    T = FunctionSpace(mesh, "HDiv Trace", (0, 0))
+    W = V * U
+
+    x, y, z = SpatialCoordinate(mesh)
+    t = Function(T)
+    expr = (1+12*pi*pi)*cos(2*pi*x)*cos(2*pi*y)*cos(2*pi*z)
+    t.interpolate(expr)
+
+    u, p = TrialFunctions(W)
+    v, q = TestFunctions(W)
+    gammar = TestFunction(T)
+
+    a_dx = (dot(u, v) + div(v)*p + q*div(u) + p*q)*dx
+    a_dS = t*dot(u*q, n)*(dS_v + dS_h)
+    a = a_dx + a_dS
+
+    tr = gammar('+')*dot(u, n)*(dS_v + dS_h)
+
+    A = Tensor(a)
+    K = Tensor(tr)
+
+    S = assemble(K * A.inv * K.T)
+    S.force_evaluation()
 
 else:
-    RT = FiniteElement("RT", triangle, degree)
-    DG = FiniteElement("DG", triangle, degree - 1)
-    Te = FiniteElement("HDiv Trace", triangle, degree - 1)
+    mesh = UnitSquareMesh(2, 2, quadrilateral=qflag)
+    n = FacetNormal(mesh)
 
-V = FunctionSpace(mesh, BrokenElement(RT))
-U = FunctionSpace(mesh, DG)
-T = FunctionSpace(mesh, Te)
+    if qflag:
+        RT = FiniteElement("RTCF", quadrilateral, degree)
+        DG = FiniteElement("DQ", quadrilateral, degree - 1)
+        Te = FiniteElement("HDiv Trace", quadrilateral, degree - 1)
 
-W = V * U
+    else:
+        RT = FiniteElement("RT", triangle, degree)
+        DG = FiniteElement("DG", triangle, degree - 1)
+        Te = FiniteElement("HDiv Trace", triangle, degree - 1)
 
-u, p = TrialFunctions(W)
-v, q = TestFunctions(W)
-gammar = TestFunction(T)
+        V = FunctionSpace(mesh, BrokenElement(RT))
+        U = FunctionSpace(mesh, DG)
+        T = FunctionSpace(mesh, Te)
 
-t = Function(T).assign(3.14)
+        W = V * U
 
-a_dx = (dot(u, v) + div(v)*p + q*div(u) + p*q)*dx
-a_dS = t*dot(u*q, n)*dS
-a = a_dx + a_dS
+        u, p = TrialFunctions(W)
+        v, q = TestFunctions(W)
+        gammar = TestFunction(T)
 
-tr = gammar('+')*dot(u, n)*dS
+        t = Function(T).assign(3.14)
 
-A = Tensor(a)
-K = Tensor(tr)
+        a_dx = (dot(u, v) + div(v)*p + q*div(u) + p*q)*dx
+        a_dS = t*dot(u*q, n)*dS
+        a = a_dx + a_dS
 
-S = assemble(K * A.inv * K.T)
-S.force_evaluation()
+        tr = gammar('+')*dot(u, n)*dS
+
+        A = Tensor(a)
+        K = Tensor(tr)
+
+        S = assemble(K * A.inv * K.T)
+        S.force_evaluation()
