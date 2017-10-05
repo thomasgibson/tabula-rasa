e665db011c9c6fa4d8f5989d5623bfd0998fff22
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-10-05 13:59:06 +0100

    Working static condensation

--- cg_static_condensation/cg_static_condense.py
+++ cg_static_condensation/cg_static_condense.py
@@ -54,10 +54,26 @@ u_h = Function(V, name="Approximate")
 
 shapes = (V_o.finat_element.space_dimension(), np.prod(V_o.shape),
           V_d.finat_element.space_dimension(), np.prod(V_d.shape))
+
+# Offset for interior dof mapping is determined by inspecting the entity
+# dofs of V (original FE space) and the dofs of V_o. For example,
+# degree 5 CG element has entity dofs:
+#
+# {0: {0: [0], 1: [1], 2: [2]}, 1: {0: [3, 4, 5, 6], 1: [7, 8, 9, 10],
+#  2: [11, 12, 13, 14]}, 2: {0: [15, 16, 17, 18, 19, 20]}}.
+#
+# Looking at the cell dofs, we have a starting dof index of 15. The
+# interior element has dofs:
+#
+# {0: {0: [], 1: [], 2: []}, 1: {0: [], 1:[], 2:[]},
+#  2: {0: [0, 1, 2, 3, 4, 5]}}
+#
+# with a starting dof index of 0. So the par_loop will need to be adjusted
+# by the difference: i + 15. The skeleton dofs do no need any offsets.
 kernel = """
 for (int i=0; i<%d; ++i){
     for (int j=0; j<%d; ++j) {
-        u_h[i][j] = u_o[i][j];
+        u_h[i+15][j] = u_o[i][j];
 }}
 
 for (int i=0; i<%d; ++i){
