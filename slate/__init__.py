041e35b6522091f2a0d17e04deeeeb5300e93c8b
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-10-09 15:15:39 +0100

    Clean out the repo for targeted experiments

--- slate/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import absolute_import, print_function, division
-from slate.slate import *            # noqa: F401
-from slate.preconditioners import *  # noqa: F401
--- slate/slac/__init__.py
+++
@@ -1,2 +0,0 @@
-from __future__ import absolute_import, print_function, division
-from slate.slac.compiler import *  # noqa: F401
