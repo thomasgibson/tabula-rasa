6e319bb7685710674078ecd89ef2bdbafe48ce1f
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-11-02 11:34:31 +0000

    Use new static condensation PC for h-dg Poisson

--- hybridization-dg/hdg-poisson.py
+++ hybridization-dg/hdg-poisson.py
@@ -1,6 +1,6 @@
 from firedrake import *
 
-degree = 1
+degree = 2
 res = 3
 mesh = UnitSquareMesh(2**res, 2**res)
 x = SpatialCoordinate(mesh)
@@ -27,23 +27,27 @@ def both(a):
     return 2*avg(a)
 
 
-a = ((dot(v, q) - div(v)*u)*dx
-     + both(uhat*inner(v, n))*dS
-     + uhat*inner(v, n)*ds
-     + dot(grad(w), q)*dx
-     - both(inner(qhat, n)*w)*dS
-     - inner(qhat, n)*w*ds
-     + both(mu*inner(qhat, n))*dS
-     + mu*inner(qhat, n)*ds)
+a = (
+    (dot(v, q) - div(v)*u)*dx
+    + both(uhat*inner(v, n))*dS
+    # + uhat*inner(v, n)*ds
+    + dot(grad(w), q)*dx
+    - both(inner(qhat, n)*w)*dS
+    - inner(qhat, n)*w*ds
+    + both(mu*inner(qhat, n))*dS
+    # + mu*inner(qhat, n)*ds
+)
 
 L = -w*f*dx
 w = Function(W, name="solutions")
-solve(a == L, w, bcs=bcs, solver_parameters={'pc_type': 'lu',
-                                             'ksp_max_it': 4,
-                                             'ksp_monitor': True,
-                                             'mat_type': 'aij',
-                                             'ksp_type': 'gmres',
-                                             'pc_factor_mat_solver_package': 'mumps'})
+params = {'mat_type': 'matfree',
+          'ksp_type': 'gmres',
+          'pc_type': 'python',
+          'ksp_monitor': True,
+          'pc_python_type': 'firedrake.HybridStaticCondensationPC',
+          'hybrid_sc': {'ksp_type': 'preonly',
+                        'pc_type': 'lu'}}
+solve(a == L, w, bcs=bcs, solver_parameters=params)
 q_h, u_h, uhat_h = w.split()
 analytic = Function(V, name="Analytical Scalar")
 analytic.interpolate(x[0]*(1-x[0])*x[1]*(1-x[1]))
