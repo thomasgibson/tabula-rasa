bb0613ff38dbb61aee9f6ff1091461b0f4a7aefb
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-11-10 15:39:29 +0000

    Update problem details

--- cg-static-condensation/cg_static_condense.py
+++ cg-static-condensation/cg_static_condense.py
@@ -1,7 +1,7 @@
 from firedrake import *
 
 
-def run_helmholtz_3D_example(d, r, alpha=None, beta=None):
+def run_helmholtz_3D_example(d, r, write=False):
     """Solves the Helmholtz equation with homogenous Neumman
     conditions imposed on the domain boundary.
     """
@@ -16,51 +16,57 @@ def run_helmholtz_3D_example(d, r, alpha=None, beta=None):
 
     # Forcing function
     f = Function(V)
-    f.interpolate((1 + 12*pi*pi)*cos(2*pi*x[0])*cos(2*pi*x[1])*cos(2*pi*x[2]))
-
-    # Coefficients
-    if alpha is None:
-        alpha = 1
-    if beta is None:
-        beta = 1
+    f.interpolate((1 + 108*pi*pi)*cos(6*pi*x[0])*cos(6*pi*x[1])*cos(6*pi*x[2]))
 
     # Bilinear and linear forms
-    a = inner(grad(v), alpha*grad(u))*dx + v*beta*u*dx
+    a = inner(grad(v), grad(u))*dx + v*u*dx
     L = v*f*dx
 
     # Solve with static condensation
     params = {'mat_type': 'matfree',
-              'ksp_type': 'preonly',
+              'ksp_type': 'gmres',
+              'ksp_monitor': True,
               'pc_type': 'python',
               'pc_python_type': 'firedrake.StaticCondensationPC',
               'static_condensation': {'ksp_type': 'cg',
-                                      'pc_type': 'ilu',
+                                      'pc_type': 'hypre',
+                                      'pc_hypre_type': 'boomeramg',
+                                      'pc_hypre_boomeramg_P_max': 4,
+                                      'ksp_monitor': True,
                                       'ksp_rtol': 1e-8}}
     u_h_sc = Function(V, name="approx-sc")
     solve(a == L, u_h_sc, solver_parameters=params)
 
     # Solve again without static condensation
     params = {'ksp_type': 'cg',
-              'pc_type': 'ilu',
+              'pc_type': 'hypre',
+              'pc_hypre_type': 'boomeramg',
+              'pc_hypre_boomeramg_P_max': 4,
+              'ksp_monitor': True,
               'ksp_rtol': 1e-8}
     u_h = Function(V, name="approx-no-sc")
     solve(a == L, u_h, solver_parameters=params)
 
     # Compare with exact solution
-    exact = Function(V, name="exact")
-    exact.interpolate(cos(2*pi*x[0])*cos(2*pi*x[1])*cos(2*pi*x[2]))
+    V_a = FunctionSpace(mesh, "CG", d + 4)
+    exact = Function(V_a, name="exact")
+    exact.interpolate(cos(6*pi*x[0])*cos(6*pi*x[1])*cos(6*pi*x[2]))
     error_sc_exact = errornorm(u_h_sc, exact)
 
     # Compare two solutions (should close)
     error_solutions = errornorm(u_h_sc, u_h)
 
+    if write:
+        File("3DSC.pvd").write(u_h_sc, exact)
+
     return u_h_sc, u_h, exact, error_sc_exact, error_solutions
 
 # Run problem
 degree = 4
-resolution = 3
+resolution = 5
 u_h_sc, u_h, u, err, err_comp = run_helmholtz_3D_example(d=degree,
-                                                         r=resolution)
+                                                         r=resolution,
+                                                         write=True)
 
 print("Error between static condensation solution and exact: %.6f"
       % err)
