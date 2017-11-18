7fd0dd7dd1a57f52eb8d8170d135c2947ea8671a
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-11-18 23:28:18 +0000

    Setup linear solver and timestepper

---
+++ swe-williamson-5/linear_solver.py
@@ -0,0 +1,127 @@
+from firedrake import *
+from gusto.linear_solvers import TimesteppingSolver
+
+
+class LinearizedShallowWaterSolver(TimesteppingSolver):
+    """
+    """
+
+    def __init__(self, state, hybridization=False,
+                 verification=False, profiling=False):
+
+        if hybridization:
+            solver_parameters = self._hybrid_params
+        else:
+            solver_parameters = self._approx_sc_params
+
+        if verification:
+            # if verification mode is on, then wrap outer solve
+            # in a GMRES loop and monitor the problem residual
+            solver_parameters['ksp_monitor_true_residual'] = True
+            solver_parameters['ksp_type'] = 'gmres'
+            solver_parameters['ksp_monitor'] = True
+
+        super(LinearizedShallowWaterSolver, self).__init__(state,
+                                                           solver_parameters=solver_parameters,
+                                                           overwrite_solver_parameters=False)
+
+        self._hybridized = hybridization
+        self._profiling = profiling
+
+    @property
+    def _hybrid_params(self):
+        return {'ksp_type': 'preonly',
+                'mat_type': 'matfree',
+                'pc_type': 'python',
+                'pc_python_type': 'firedrake.HybridizationPC',
+                'hybridization': {'ksp_type': 'cg',
+                                  'pc_type': 'gamg',
+                                  'ksp_monitor': True,
+                                  'mg_levels_ksp_type': 'chebyshev',
+                                  'mg_levels_ksp_max_it': 2,
+                                  'mg_levels_pc_type': 'bjacobi',
+                                  'mg_levels_sub_pc_type': 'ilu',
+                                  # Construct broken residual
+                                  'hdiv_residual': {'ksp_type': 'cg',
+                                                    'pc_type': 'bjacobi',
+                                                    'sub_pc_type': 'ilu',
+                                                    'ksp_rtol': 1e-8,
+                                                    'ksp_monitor': True},
+                                  # Reconstruct HDiv vector field
+                                  # via local averaging
+                                  # (Alternatively, one could also use
+                                  # a Galerkin projection onto the HDiv space)
+                                  # 'hdiv_projection':{'ksp_type': 'cg',
+                                  #                    'pc_type': 'bjacobi',
+                                  #                    'sub_pc_type': 'ilu',
+                                  #                    'ksp_rtol': 1e-8,
+                                  #                    'ksp_monitor': True}}}
+                                  'hdiv_projection': {'method': 'average'}}}
+
+    @property
+    def _approx_sc_params(self):
+        return {'pc_type': 'fieldsplit',
+                'pc_fieldsplit_type': 'schur',
+                'ksp_type': 'gmres',
+                'ksp_monitor': True,
+                'ksp_max_it': 100,
+                'ksp_gmres_restart': 50,
+                'pc_fieldsplit_schur_fact_type': 'FULL',
+                'pc_fieldsplit_schur_precondition': 'selfp',
+                'fieldsplit_0': {'ksp_type': 'preonly',
+                                 'pc_type': 'bjacobi',
+                                 'sub_pc_type': 'ilu'},
+                # This is the reduced system for the depth field.
+                # We apply the same solver options as the hybridized
+                # reduced system for the Lagrange multipliers.
+                'fieldsplit_1': {'ksp_type': 'cg',
+                                 'pc_type': 'gamg',
+                                 'ksp_monitor': True,
+                                 'mg_levels': {'ksp_type': 'chebyshev',
+                                               'ksp_max_it': 2,
+                                               'pc_type': 'bjacobi',
+                                               'sub_pc_type': 'ilu'}}}
+
+    def _setup_solver(self):
+        state = self.state
+        H = state.parameters.H
+        g = state.parameters.g
+        beta = state.timestepping.dt*state.timestepping.alpha
+
+        # Split up the rhs vector (symbolically)
+        u_in, D_in = split(state.xrhs)
+
+        W = state.W
+        w, phi = TestFunctions(W)
+        u, D = TrialFunctions(W)
+
+        # Linearized shallow water system in residual form
+        eqn = (inner(w, u) - beta*g*div(w)*D
+               - inner(w, u_in)
+               + phi*D + beta*H*phi*div(u)
+               - phi*D_in)*dx
+
+        aeqn = lhs(eqn)
+        Leqn = rhs(eqn)
+
+        # Place to put result of u D solver
+        self.uD = Function(W)
+
+        # Solver for u, D
+        uD_problem = LinearVariationalProblem(
+            aeqn, Leqn, self.state.dy)
+
+        if self.hybridized:
+            prefix = 'SWImplicitHybridMixed'
+        else:
+            prefix = 'SWImplicit'
+
+        self.uD_solver = LinearVariationalSolver(uD_problem,
+                                                 solver_parameters=self.solver_parameters,
+                                                 options_prefix=prefix)
+
+    def solve(self):
+        """
+        Apply the solver with rhs state.xrhs and result state.dy.
+        """
+        self.uD_solver.solve()
--- swe-williamson-5/sw_williamson5.py
+++ swe-williamson-5/sw_williamson5.py
@@ -1,100 +1,92 @@
 from gusto import *
 from firedrake import IcosahedralSphereMesh, SpatialCoordinate, \
-    as_vector, pi, sqrt, Min, FunctionSpace
-import sys
+    as_vector, pi, sqrt, Min, FunctionSpace, COMM_WORLD, parameters
+from firedrake.petsc import PETSc
+from crank_nicolson import CrankNicolsonStepper
+from linear_solver import LinearizedShallowWaterSolver
+from argparse import ArgumentParser
+
+import pandas as pd
 
 
 parameters["pyop2_options"]["lazy_evaluation"] = False
 
 
-def is_intstring(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
+PETSc.Log.begin()
+parser = ArgumentParser(description="""Run Williamson test case 5""",
+                        add_help=False)
 
-# Use a specific dumpfreq
-if is_intstring(sys.argv[1]):
-    dumpfreq = int(sys.argv[1])
+parser.add_argument("--hybridization",
+                    default=False,
+                    type=bool,
+                    action="store",
+                    help="Select 'True' or 'False'. Default is 'False'.")
+
+parser.add_argument("--testing",
+                    default=False,
+                    type=bool,
+                    action="store",
+                    help=("Select 'True' or 'False' to enable a test run. "
+                          "Default is False."))
+
+parser.add_argument("--dumpfreq",
+                    default=0,
+                    action="store",
+                    help="Dump frequency of output.")
+
+parser.add_argument("--refinements",
+                    action="store",
+                    default=3,
+                    type=int,
+                    choices=[3, 4, 5, 6, 7],
+                    help="How many refinements to make to the sphere mesh.")
+
+parser.add_argument("--profile",
+                    action="store",
+                    default=False,
+                    type=bool,
+                    help="Turn profiling on? This forces 7 mesh refinements.")
+
+parser.add_argument("--help",
+                    action="store_true",
+                    help="Show help")
+
+args, _ = parser.parse_known_args()
+
+if args.hybridization is not None:
+    if args.hybridization not in [True, False]:
+        raise ValueError("Unrecognized argument '%s', use a boolean"
+                         % args.hybridization)
+    hybridize = args.hybridization
+
 else:
-    dumpfreq = 100
+    hybridize = False
+
 
 day = 24.*60.*60.
-if '--running-tests' in sys.argv:
+if args.testing:
     ref_dt = {3: 3000.}
     tmax = 3000.
-elif '--run-test-lvl3' in sys.argv:
-    ref_dt = {3: 900.}
-    tmax = 50*day
-elif '--run-test-lvl4' in sys.argv:
-    ref_dt = {4: 450.}
-    tmax = 50*day
-elif '--run-test-lvl5' in sys.argv:
-    ref_dt = {5: 225.}
-    tmax = 50*day
-elif '--run-test-lvl6' in sys.argv:
-    ref_dt = {6: 112.5}
-    tmax = 50*day
-elif '--run-test-lvl7' in sys.argv:
-    ref_dt = {7: 56.25}
-    tmax = 50*day
-elif '--profile-mode' in sys.argv:
+elif args.profiling:
     ref_dt = {7: 56.25}
     tmax = 1125.
-else:
-    # setup resolution and timestepping parameters for convergence test
-    ref_dt = {3: 900., 4: 450., 5: 225., 6: 112.5}
+elif args.refinements == 3:
+    ref_dt = {3: 900.}
     tmax = 50*day
-
-# Turn hybridization on/off
-if '--hybrid-mixed-method' in sys.argv:
-    params = {'ksp_type': 'preonly',
-              'mat_type': 'matfree',
-              'pc_type': 'python',
-              'pc_python_type': 'firedrake.HybridizationPC',
-              'hybridization': {'ksp_type': 'cg',
-                                'pc_type': 'gamg',
-                                'ksp_monitor': True,
-                                'mg_levels_ksp_type': 'chebyshev',
-                                'mg_levels_ksp_max_it': 2,
-                                'mg_levels_pc_type': 'bjacobi',
-                                'mg_levels_sub_pc_type': 'ilu',
-                                # Construct broken residual
-                                'hdiv_residual': {'ksp_type': 'cg',
-                                                  'pc_type': 'bjacobi',
-                                                  'sub_pc_type': 'ilu',
-                                                  'ksp_rtol': 1e-8,
-                                                  'ksp_monitor': True},
-                                # Reconstruct HDiv vector field
-                                # via local averaging
-                                # (Alternatively, one could also use
-                                # a Galerkin projection onto the HDiv space)
-                                # 'hdiv_projection':{'ksp_type': 'cg',
-                                #                    'pc_type': 'bjacobi',
-                                #                    'sub_pc_type': 'ilu',
-                                #                    'ksp_rtol': 1e-8,
-                                #                    'ksp_monitor': True}}}
-                                'hdiv_projection': {'method': 'average'}}}
+elif args.refinements == 4:
+    ref_dt = {4: 450.}
+    tmax = 50*day
+elif args.refinements == 5:
+    ref_dt = {5: 225.}
+    tmax = 50*day
+elif args.refinements == 6:
+    ref_dt = {6: 112.5}
+    tmax = 50*day
+elif args.refinements == 7:
+    ref_dt = {7: 56.25}
+    tmax = 50*day
 else:
-    params = {'pc_type': 'fieldsplit',
-              'pc_fieldsplit_type': 'schur',
-              'ksp_type': 'gmres',
-              'ksp_monitor': True,
-              'ksp_max_it': 100,
-              'ksp_gmres_restart': 50,
-              'pc_fieldsplit_schur_fact_type': 'FULL',
-              'pc_fieldsplit_schur_precondition': 'selfp',
-              'fieldsplit_0_ksp_type': 'preonly',
-              'fieldsplit_0_pc_type': 'bjacobi',
-              'fieldsplit_0_sub_pc_type': 'ilu',
-              'fieldsplit_1_ksp_type': 'cg',
-              'fieldsplit_1_pc_type': 'gamg',
-              'fieldsplit_1_ksp_monitor': True,
-              'fieldsplit_1_mg_levels_ksp_type': 'chebyshev',
-              'fieldsplit_1_mg_levels_ksp_max_it': 2,
-              'fieldsplit_1_mg_levels_pc_type': 'bjacobi',
-              'fieldsplit_1_mg_levels_sub_pc_type': 'ilu'}
+    raise ValueError("What?")
 
 # Setup shallow water parameters
 R = 6371220.
@@ -107,7 +99,11 @@ diagnostics = Diagnostics(*fieldlist)
 
 for ref_level, dt in ref_dt.items():
 
-    dirname = "sw_W5_ref%s_dt%s" % (ref_level, dt)
+    if hybridized:
+        dirname = "results/hybrid_sw_W5_ref%s_dt%s" % (ref_level, dt)
+    else:
+        dirname = "results/sw_W5_ref%s_dt%s" % (ref_level, dt)
+
     mesh = IcosahedralSphereMesh(radius=R,
                                  refinement_level=ref_level, degree=3)
     x = SpatialCoordinate(mesh)
@@ -173,17 +169,35 @@ for ref_level, dt in ref_dt.items():
     advected_fields.append(("u", ThetaMethod(state, u0, ueqn)))
     advected_fields.append(("D", SSPRK3(state, D0, Deqn)))
 
-    linear_solver = ShallowWaterSolver(state,
-                                       solver_parameters=params,
-                                       overwrite_solver_parameters=True)
+    linear_solver = LinearizedShallowWaterSolver(state)
 
     # Set up forcing
     sw_forcing = ShallowWaterForcing(state, euler_poincare=False)
 
     # Build time stepper using Crank-Nicolson
-    stepper = CrankNicolson(state,
-                            advected_fields,
-                            linear_solver,
-                            sw_forcing)
+    stepper = CrankNicolsonStepper(state,
+                                   advected_fields,
+                                   linear_solver,
+                                   sw_forcing)
 
     stepper.run(t=0, tmax=tmax)
+
+    if COMM_WORLD.rank == 0:
+        if hybridized:
+            if args.profile:
+                results = "hybrid_profiling_sw_W5_ref%s_dt%s" % (ref_level, dt)
+            else:
+                results = "hybrid_sw_W5_ref%s_dt%s" % (ref_level, dt)
+        else:
+            if args.profile:
+                results = "profiling_sw_W5_ref%s_dt%s" % (ref_level, dt)
+            else:
+                results = "sw_W5_ref%s_dt%s" % (ref_level, dt)
+
+        data = {"Timestep": stepper.t_array,
+                "SolveTime": stepper.solve_time_array,
+                "OuterKSPIterations": stepper.ksp_iter_array,
+                "InnerKSPIterations": stepper.inner_ksp_iter_array}
+
+        df = pd.DataFrame(data)
+        df.to_csv(results, index=False, mode="w", header=True)
