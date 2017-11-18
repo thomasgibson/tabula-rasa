3dff256362e7d6abda12f746bab4f7ae4e68db2c
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-11-18 17:13:13 +0000

    Add scripts for each convergence history table for LDGH methods

---
+++ results/ldg-h-tau-order-hneg1-table.py
@@ -0,0 +1,91 @@
+import csv
+
+
+tex_file = open("table-ldg-h-tau-order-hneg1.tex", "w")
+
+
+table = r"""
+\begin{table}
+    \centering
+    \caption{Convergence history for the LDG-H method for $\tau = \frac{1}{h}$.}
+    \resizebox{\textwidth}{!}{%
+        \begin{tabular}{l c c c c c c c c c}
+            \toprule
+            \multicolumn{10}{c}{LDG-H method ($\tau = \mathcal{O}(h^{-1})$)} \\
+            \cmidrule{2-10}
+            \multirow{2}{*}{$k$} & mesh &
+            \multicolumn{2}{c}{$\norm{p-p_h}_{L^2(\Omega)} \leq \mathcal{O}(h^{k+1})$} &
+            \multicolumn{2}{c}{
+                $\norm{\boldsymbol{u}-\boldsymbol{u}_h}_{\boldsymbol{L}^2(\Omega)} \leq \mathcal{O}(h^{k})$} &
+            \multicolumn{2}{c}{$\norm{p-p_h^{\star}}_{L^2(\Omega)} \leq \mathcal{O}(h^{k+1})$} &
+            \multicolumn{2}{c}{
+                $\norm{\boldsymbol{u}-\boldsymbol{u}_h^{\star}}_{\boldsymbol{L}^2(\Omega)} \leq \mathcal{O}(h^{k})$} \\
+            & $r$ & error & order & error & order & error & order & error & order \\
+            \bottomrule
+            \multirow{6}{*}{1}
+"""
+
+# Now we're ready to starting writing in row-by-row
+# starting with degree = 1:
+with open("LDG-H/LDG-H-d1-tau_order-hneg1.csv") as csvfile3:
+    readCSV = csv.reader(csvfile3, delimiter=",")
+    for j, row in enumerate(readCSV):
+        # Ignore first row --- these are just column labels
+        if j == 0:
+            continue
+        else:
+            # Number of columns are labeled 0, ..., n-1.
+            for i in range(0, 9):
+                # Final column, need to insert 'next line' in table.
+                if i == 8:
+                    table += " & " + str(row[i]) + " \\\\ \n"
+                else:
+                    table += " & " + str(row[i])
+
+# Now we're finished reading from this file. Start the next one and so on.
+table += r"""
+\midrule
+\multirow{6}{*}{2}
+"""
+with open("LDG-H/LDG-H-d2-tau_order-hneg1.csv") as csvfile4:
+    readCSV = csv.reader(csvfile4, delimiter=",")
+    for j, row in enumerate(readCSV):
+        # Ignore first row --- these are just column labels
+        if j == 0:
+            continue
+        else:
+            # Number of columns are labeled 0, ..., n-1.
+            for i in range(0, 9):
+                # Final column, need to insert 'next line' in table.
+                if i == 8:
+                    table += " & " + str(row[i]) + " \\\\ \n"
+                else:
+                    table += " & " + str(row[i])
+
+table += r"""
+\midrule
+\multirow{6}{*}{3}
+"""
+with open("LDG-H/LDG-H-d3-tau_order-hneg1.csv") as csvfile5:
+    readCSV = csv.reader(csvfile5, delimiter=",")
+    for j, row in enumerate(readCSV):
+        # Ignore first row --- these are just column labels
+        if j == 0:
+            continue
+        else:
+            # Number of columns are labeled 0, ..., n-1.
+            for i in range(0, 9):
+                # Final column, need to insert 'next line' in table.
+                if i == 8:
+                    table += " & " + str(row[i]) + " \\\\ \n"
+                else:
+                    table += " & " + str(row[i])
+
+table += r"""
+        \bottomrule
+    \end{tabular}}
+\end{table}
+"""
+
+tex_file.write(table)
+tex_file.close()
