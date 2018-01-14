64c226977b5d8bf5d1c545055deda5a189f68b67
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-01-14 23:40:26 +0000

    Update LDG-H table scripts

--- results/ldg-h-tau-order-h-table.py
+++ results/ldg-h-tau-order-h-table.py
@@ -1,91 +1,66 @@
-import csv
+import os
+import sys
+import pandas as pd
 
 
-tex_file = open("table-ldg-h-tau-order-h.tex", "w")
+data_set = ["LDG-H/LDG-H-d1-tau_order-h.csv",
+            "LDG-H/LDG-H-d2-tau_order-h.csv",
+            "LDG-H/LDG-H-d3-tau_order-h.csv"]
+
+for data in data_set:
+    if not os.path.exists(data):
+        print("Cannot find data file '%s'" % data)
+        sys.exit(1)
 
 
-table = r"""
-\begin{table}
-    \centering
-    \caption{Convergence history for the LDG-H method for $\tau = h$.}
-    \resizebox{\textwidth}{!}{%
-        \begin{tabular}{l c c c c c c c c c}
-            \toprule
-            \multicolumn{10}{c}{LDG-H method ($\tau = \mathcal{O}(h)$)} \\
-            \cmidrule{2-10}
-            \multirow{2}{*}{$k$} & mesh &
-            \multicolumn{2}{c}{$\norm{p-p_h}_{L^2(\Omega)} \leq \mathcal{O}(h^{k})$} &
-            \multicolumn{2}{c}{
-                $\norm{\boldsymbol{u}-\boldsymbol{u}_h}_{\boldsymbol{L}^2(\Omega)} \leq \mathcal{O}(h^{k+1})$} &
-            \multicolumn{2}{c}{$\norm{p-p_h^{\star}}_{L^2(\Omega)} \leq \mathcal{O}(h^{k+2})$} &
-            \multicolumn{2}{c}{
-                $\norm{\boldsymbol{u}-\boldsymbol{u}_h^{\star}}_{\boldsymbol{L}^2(\Omega)} \leq \mathcal{O}(h^{k+1}) $} \\
-            & $r$ & error & order & error & order & error & order & error & order \\
-            \bottomrule
-            \multirow{6}{*}{1}
+table = r"""\resizebox{\textwidth}{!}{%
+\begin{tabular}{| l | c | c | c | c | c | c | c | c | c |}
+\hline
+\multicolumn{10}{|c|}{LDG-H method ($\tau = \mathcal{O}(h)$)} \\
+\hline
+\multirow{2}{*}{$k$} & mesh &
+\multicolumn{2}{|c|}{$\norm{p-p_h}_{L^2(\Omega)} \leq \mathcal{O}(h^{k})$} &
+\multicolumn{2}{|c|}{
+$\norm{\boldsymbol{u}-\boldsymbol{u}_h}_{\boldsymbol{L}^2(\Omega)} \leq \mathcal{O}(h^{k+1})$} &
+\multicolumn{2}{|c|}{$\norm{p-p_h^{\star}}_{L^2(\Omega)} \leq \mathcal{O}(h^{k+2})$} &
+\multicolumn{2}{|c|}{
+$\norm{\boldsymbol{u}-\boldsymbol{u}_h^{\star}}_{\boldsymbol{L}^2(\Omega)} \leq \mathcal{O}(h^{k+1})$} \\
+\cline{2-10}
+& $r$ & $L^2$-error & rate & $L^2$-error & rate & $L^2$-error & rate & $L^2$-error & rate \\
 """
 
-# Now we're ready to starting writing in row-by-row
-# starting with degree = 1:
-with open("LDG-H/LDG-H-d1-tau_order-h.csv") as csvfile0:
-    readCSV = csv.reader(csvfile0, delimiter=",")
-    for j, row in enumerate(readCSV):
-        # Ignore first row --- these are just column labels
-        if j == 0:
-            continue
-        else:
-            # Number of columns are labeled 0, ..., n-1.
-            for i in range(0, 9):
-                # Final column, need to insert 'next line' in table.
-                if i == 8:
-                    table += " & " + str(row[i]) + " \\\\ \n"
-                else:
-                    table += " & " + str(row[i])
-
-# Now we're finished reading from this file. Start the next one and so on.
-table += r"""
-\midrule
-\multirow{6}{*}{2}
-"""
-with open("LDG-H/LDG-H-d2-tau_order-h.csv") as csvfile1:
-    readCSV = csv.reader(csvfile1, delimiter=",")
-    for j, row in enumerate(readCSV):
-        # Ignore first row --- these are just column labels
-        if j == 0:
-            continue
-        else:
-            # Number of columns are labeled 0, ..., n-1.
-            for i in range(0, 9):
-                # Final column, need to insert 'next line' in table.
-                if i == 8:
-                    table += " & " + str(row[i]) + " \\\\ \n"
-                else:
-                    table += " & " + str(row[i])
-
-table += r"""
-\midrule
-\multirow{6}{*}{3}
-"""
-with open("LDG-H/LDG-H-d3-tau_order-h.csv") as csvfile2:
-    readCSV = csv.reader(csvfile2, delimiter=",")
-    for j, row in enumerate(readCSV):
-        # Ignore first row --- these are just column labels
-        if j == 0:
-            continue
-        else:
-            # Number of columns are labeled 0, ..., n-1.
-            for i in range(0, 9):
-                # Final column, need to insert 'next line' in table.
-                if i == 8:
-                    table += " & " + str(row[i]) + " \\\\ \n"
-                else:
-                    table += " & " + str(row[i])
-
-table += r"""
-        \bottomrule
-    \end{tabular}}
-\end{table}
+lformat = r"""& {mesh: d} & {ScalarErrors:.3e} & {ScalarRates} & {FluxErrors:.3e} & {FluxRates} & {PPScalarErrors:.3e} & {PPScalarRates} & {PPFluxErrors:.3e} & {PPFluxRates} \\
 """
 
-tex_file.write(table)
-tex_file.close()
+
+def rate(s):
+    if s == '---':
+        return s
+    else:
+        return "{s:.3f}".format(s=float(s))
+
+
+for data in data_set:
+    df = pd.read_csv(data)
+    df = df.sort_values("Mesh")
+    degree = df.Degree.values[0]
+    table += r"""
+    \hline
+    \multirow{6}{*}{%d}
+    """ % degree
+    for k in df.Mesh:
+        sliced = df.loc[lambda x: x.Mesh == k]
+        table += lformat.format(mesh=k,
+                                ScalarErrors=sliced.ScalarErrors.values[0],
+                                ScalarRates=rate(sliced.ScalarConvRates.values[0]),
+                                FluxErrors=sliced.FluxErrors.values[0],
+                                FluxRates=rate(sliced.FluxConvRates.values[0]),
+                                PPScalarErrors=sliced.PostProcessedScalarErrors.values[0],
+                                PPScalarRates=rate(sliced.PostProcessedScalarRates.values[0]),
+                                PPFluxErrors=sliced.PostProcessedFluxErrors.values[0],
+                                PPFluxRates=rate(sliced.PostProcessedFluxRates.values[0]))
+
+table += r"""\hline
+\end{tabular}}
+"""
+print(table)
