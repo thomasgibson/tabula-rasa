3b855abf58748b585c5ccaf52ce5c1ba08809d58
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-04-28 14:12:47 +0100

    Updated parameter set for CG

--- HDG_CG_comp/run_cg.py
+++ HDG_CG_comp/run_cg.py
@@ -131,23 +131,25 @@ if dim == 3:
     # (degree, size, rtol) NOTE: rtol is chosen such that the
     # iterative solver reaches the minimal algebraic error
     # so that we avoid "oversolving"
-    cg_params = [(2, 8, 1.0e-5),
+    cg_params = [(2, 4, 1.0e-4),
+                 (2, 8, 1.0e-5),
                  (2, 16, 1.0e-6),
                  (2, 32, 1.0e-7),
                  (2, 64, 1.0e-8),
                  (2, 128, 1.0e-9),
                  # Degree 3 set
+                 (3, 4, 1.0e-6),
                  (3, 8, 1.0e-7),
                  (3, 16, 1.0e-8),
                  (3, 32, 1.0e-9),
                  (3, 64, 1.0e-10),
                  (3, 128, 1.0e-11),
                  # Degree 4 set
+                 (4, 4, 1.0e-8),
                  (4, 8, 1.0e-9),
                  (4, 16, 1.0e-10),
                  (4, 32, 1.0e-11),
-                 (4, 64, 1.0e-12),
-                 (4, 128, 1.0e-13)]
+                 (4, 64, 1.0e-12)]
 else:
     raise NotImplementedError("Dim %s not set up yet." % dim)
 
