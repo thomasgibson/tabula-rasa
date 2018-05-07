36d49fffd1197d3b57ae90aec85e3f6edadd8357
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-05-07 14:47:36 +0100

    Bar plots for SWE tests

--- results/plot_williamson_solves.py
+++ results/plot_williamson_solves.py
@@ -3,12 +3,16 @@ import sys
 import pandas as pd
 import numpy as np
 import seaborn
+import matplotlib
 
 from matplotlib import pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 
 
-params = [("RTCF", 1, 8, 28.125),
-          ("RTCF", 2, 8, 28.125),
+params = [("RT", 1, 8, 28.125),
+          # ("RT", 2, 8, 28.125),
+          # ("RTCF", 1, 8, 28.125),
+          # ("RTCF", 2, 8, 28.125),
           ("BDM", 2, 8, 28.125)]
 gmres_times = ["SWE/gmres_%s%s_profile_W5_ref%d_Dt%s_NS100.csv" % param
                for param in params]
@@ -23,117 +27,384 @@ for data in hybrid_times:
 
 seaborn.set(style="ticks")
 
-fig, axes = plt.subplots(1, 2, figsize=(12, 4), squeeze=False)
+fig, axes = plt.subplots(2, 1, figsize=(4, 6), squeeze=False)
 axes = axes.flatten()
-ax0, ax = axes
+ax1, ax2 = axes
 
 hmm_dfs = [pd.read_csv(d) for d in hybrid_times]
 gmres_dfs = [pd.read_csv(d) for d in gmres_times]
 
-ind = np.arange(len(params))
-width = 0.67
+ind = np.arange(2)
+width = 0.5
 
 for ax in axes:
     ax.set_xticks(ind)
     # ax.set_yscale("log")
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    # ax.set_axisbelow(False)
+    # ax.yaxis.grid(color='gray', linestyle='--')
+    # ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter(useOffset=False))
 
-methods = []
-degrees_of_freedom = []
 method = {("RT", 1): "$RT_1 \\times DG_0$",
           ("RT", 2): "$RT_2 \\times DG_1$",
           ("BDM", 2): "$BDM_2 \\times DG_1$",
           ("RTCF", 1): "$RTCF_1 \\times DQ_0$",
           ("RTCF", 2): "$RTCF_2 \\times DQ_1$"}
 
-ax0.set_ylabel("Time (s)")
+# ax1.set_ylabel("Time [s] (log-scale)")
+# ax3.set_ylabel("Time [s] (log-scale)")
 
-schur_times = []
-schur_low_times = []
-mass_inverse_times = []
-gmres_other_times = []
-for i, df in enumerate(gmres_dfs):
+# RT on triangles
+rtlo_schur_times = []
+rtlo_schur_low_times = []
+rtlo_mass_inverse_times = []
+rtlo_gmres_other_times = []
+rtnlo_schur_times = []
+rtnlo_schur_low_times = []
+rtnlo_mass_inverse_times = []
+rtnlo_gmres_other_times = []
+
+# RT on quads
+rtcflo_schur_times = []
+rtcflo_schur_low_times = []
+rtcflo_mass_inverse_times = []
+rtcflo_gmres_other_times = []
+rtcfnlo_schur_times = []
+rtcfnlo_schur_low_times = []
+rtcfnlo_mass_inverse_times = []
+rtcfnlo_gmres_other_times = []
+
+# BDM2 on triangles
+bdm_schur_times = []
+bdm_schur_low_times = []
+bdm_mass_inverse_times = []
+bdm_gmres_other_times = []
+
+# DOFs
+rtlo_dofs = []
+rtcflo_dofs = []
+rtnlo_dofs = []
+rtcfnlo_dofs = []
+bdm_dofs = []
+
+# Time (KSPSolve)
+rtlo_time = []
+rtcflo_time = []
+rtnlo_time = []
+rtcfnlo_time = []
+bdm_time = []
+
+for df in gmres_dfs:
+
     schur_solve = df.KSPSchur.values[0]
     schur_low = df.KSPFSLow.values[0]
     mass_invert = df.KSPF0.values[0]
     other = df.PETScLogKSPSolve.values[0] - (schur_solve + schur_low
                                              + mass_invert)
+    dofs = df.total_dofs.values[0]
+    kspsolve = df.PETScLogKSPSolve.values[0]
 
-    schur_times.append(schur_solve)
-    schur_low_times.append(schur_low)
-    mass_inverse_times.append(mass_invert)
-    gmres_other_times.append(other)
+    if df.method.values[0] == "RT":
 
-a1 = ax0.bar(ind, schur_times, width,
-             color='#30a2da')
-a2 = ax0.bar(ind, mass_inverse_times, width,
-             bottom=schur_times,
-             color='#fc4f30')
-a3 = ax0.bar(ind, schur_low_times, width,
-             bottom=(np.array(schur_times) +
-                     np.array(mass_inverse_times)),
-             color='#e5ae38')
-a4 = ax0.bar(ind, gmres_other_times, width,
-             bottom=(np.array(schur_times) +
-                     np.array(mass_inverse_times) +
-                     np.array(schur_low_times)),
-             color="k")
+        if df.model_degree.values[0] == 1:
 
-ax0.legend((a1[0], a2[0], a3[0], a4[0]), ("Schur solve",
-                                          "Velocity mass",
-                                          "Apply velocity mass",
-                                          "GMRES other"))
+            rtlo_schur_times.append(schur_solve)
+            rtlo_schur_low_times.append(schur_low)
+            rtlo_mass_inverse_times.append(mass_invert)
+            rtlo_gmres_other_times.append(other)
+            rtlo_dofs.append(dofs)
+            rtlo_time.extend([schur_solve, kspsolve])
 
-rhs_assembly_times = []
-trace_solve_times = []
-local_recovery_times = []
-projection_times = []
+        else:
 
-for i, df in enumerate(hmm_dfs):
-    methods.append((df.method.values[0], df.model_degree.values[0]))
-    degrees_of_freedom.append(df.total_dofs.values[0])
+            assert df.model_degree.values[0] == 2
+
+            rtnlo_schur_times.append(schur_solve)
+            rtnlo_schur_low_times.append(schur_low)
+            rtnlo_mass_inverse_times.append(mass_invert)
+            rtnlo_gmres_other_times.append(other)
+            rtnlo_dofs.append(dofs)
+            rtnlo_time.extend([schur_solve])
+
+    elif df.method.values[0] == "RTCF":
+
+        if df.model_degree.values[0] == 1:
+
+            rtcflo_schur_times.append(schur_solve)
+            rtcflo_schur_low_times.append(schur_low)
+            rtcflo_mass_inverse_times.append(mass_invert)
+            rtcflo_gmres_other_times.append(other)
+            rtcflo_dofs.append(dofs)
+            rtcflo_time.extend([schur_solve, kspsolve])
+
+        else:
+
+            assert df.model_degree.values[0] == 2
+
+            rtcfnlo_schur_times.append(schur_solve)
+            rtcfnlo_schur_low_times.append(schur_low)
+            rtcfnlo_mass_inverse_times.append(mass_invert)
+            rtcfnlo_gmres_other_times.append(other)
+            rtcfnlo_dofs.append(dofs)
+            rtcfnlo_time.extend([schur_solve, kspsolve])
+
+    else:
+
+        assert df.method.values[0] == "BDM"
+        assert df.model_degree.values[0] == 2
+
+        bdm_schur_times.append(schur_solve)
+        bdm_schur_low_times.append(schur_low)
+        bdm_mass_inverse_times.append(mass_invert)
+        bdm_gmres_other_times.append(other)
+        bdm_dofs.append(dofs)
+        bdm_time.extend([schur_solve, kspsolve])
+
+# H-RT on triangles
+hrtlo_rhs_assembly_times = []
+hrtlo_trace_solve_times = []
+hrtlo_local_recovery_times = []
+hrtlo_projection_times = []
+hrtnlo_rhs_assembly_times = []
+hrtnlo_trace_solve_times = []
+hrtnlo_local_recovery_times = []
+hrtnlo_projection_times = []
+
+# H-RTCF on triangles
+hrtcflo_rhs_assembly_times = []
+hrtcflo_trace_solve_times = []
+hrtcflo_local_recovery_times = []
+hrtcflo_projection_times = []
+hrtcfnlo_rhs_assembly_times = []
+hrtcfnlo_trace_solve_times = []
+hrtcfnlo_local_recovery_times = []
+hrtcfnlo_projection_times = []
+
+# H-BDM2 on triangles
+hbdm_rhs_assembly_times = []
+hbdm_trace_solve_times = []
+hbdm_local_recovery_times = []
+hbdm_projection_times = []
+
+# DOFs
+hrtlo_dofs = []
+hrtcflo_dofs = []
+hrtnlo_dofs = []
+hrtcfnlo_dofs = []
+hbdm_dofs = []
+
+# Time (KSPSolve)
+hrtlo_time = []
+hrtcflo_time = []
+hrtnlo_time = []
+hrtcfnlo_time = []
+hbdm_time = []
+
+for df in hmm_dfs:
 
     break_time = df.HybridBreak.values[0]
     rhs_time = df.HybridRHS.values[0]
     rhs_assembly = break_time + rhs_time
-    rhs_assembly_times.append(rhs_assembly)
 
     trace_solve_time = df.HybridTraceSolve.values[0]
-    trace_solve_times.append(trace_solve_time)
 
     recon_time = df.HybridReconstruction.values[0]
     proj_time = df.HybridProjection.values[0]
-    local_recovery_times.append(recon_time)
-    projection_times.append(proj_time)
 
-a1 = ax.bar(ind, trace_solve_times, width)
-a2 = ax.bar(ind, rhs_assembly_times, width,
-            bottom=trace_solve_times)
-a3 = ax.bar(ind, local_recovery_times, width,
-            bottom=np.array(rhs_assembly_times) + np.array(trace_solve_times))
-a4 = ax.bar(ind, projection_times, width,
-            bottom=(np.array(rhs_assembly_times) +
-                    np.array(trace_solve_times) +
-                    np.array(local_recovery_times)))
+    ksptime = df.PETScLogKSPSolve.values[0]
+    dofs = df.total_dofs.values[0]
 
-ax.legend((a1[0], a2[0], a3[0], a4[0]), ("Linear solve",
-                                         "RHS assembly",
-                                         "Local recovery",
-                                         "Projection"))
+    if df.method.values[0] == "RT":
 
-ax0.set_xticklabels(["%s\n(%d)" % (method[mth], dofs) for (mth, dofs) in
-                     zip(methods, degrees_of_freedom)])
-ax.set_xticklabels(["%s\n(%d)" % (method[mth], dofs) for (mth, dofs) in
-                    zip(methods, degrees_of_freedom)])
+        if df.model_degree.values[0] == 1:
 
-xlabel = fig.text(0.5, -0.1,
-                  "Discretization\n(Degrees of freedom)",
+            hrtlo_rhs_assembly_times.append(rhs_assembly)
+            hrtlo_trace_solve_times.append(trace_solve_time)
+            hrtlo_local_recovery_times.append(recon_time)
+            hrtlo_projection_times.append(proj_time)
+            hrtlo_dofs.append(dofs)
+            hrtlo_time.extend([trace_solve_time,
+                               trace_solve_time + rhs_assembly,
+                               ksptime])
+
+        else:
+
+            assert df.model_degree.values[0] == 2
+
+            hrtnlo_rhs_assembly_times.append(rhs_assembly)
+            hrtnlo_trace_solve_times.append(trace_solve_time)
+            hrtnlo_local_recovery_times.append(recon_time)
+            hrtnlo_projection_times.append(proj_time)
+            hrtnlo_dofs.append(dofs)
+            hrtnlo_time.extend([trace_solve_time,
+                                trace_solve_time + rhs_assembly,
+                                ksptime])
+
+    elif df.method.values[0] == "RTCF":
+
+        if df.model_degree.values[0] == 1:
+
+            hrtcflo_rhs_assembly_times.append(rhs_assembly)
+            hrtcflo_trace_solve_times.append(trace_solve_time)
+            hrtcflo_local_recovery_times.append(recon_time)
+            hrtcflo_projection_times.append(proj_time)
+            hrtcflo_dofs.append(dofs)
+            hrtcflo_time.extend([trace_solve_time,
+                                 trace_solve_time + rhs_assembly,
+                                 ksptime])
+
+        else:
+
+            assert df.model_degree.values[0] == 2
+
+            hrtcfnlo_rhs_assembly_times.append(rhs_assembly)
+            hrtcfnlo_trace_solve_times.append(trace_solve_time)
+            hrtcfnlo_local_recovery_times.append(recon_time)
+            hrtcfnlo_projection_times.append(proj_time)
+            hrtcfnlo_dofs.append(dofs)
+            hrtcfnlo_time.extend([trace_solve_time,
+                                  trace_solve_time + rhs_assembly,
+                                  ksptime])
+
+    else:
+
+        assert df.method.values[0] == "BDM"
+        assert df.model_degree.values[0] == 2
+
+        hbdm_rhs_assembly_times.append(rhs_assembly)
+        hbdm_trace_solve_times.append(trace_solve_time)
+        hbdm_local_recovery_times.append(recon_time)
+        hbdm_projection_times.append(proj_time)
+        hbdm_time.extend([trace_solve_time,
+                          trace_solve_time + rhs_assembly,
+                          ksptime])
+
+# LO RT
+ax21 = ax1.twinx()
+ax1.set_yscale('log')
+# ax21.set_yscale('log')
+
+ag1 = ax1.bar(ind[0], rtlo_schur_times[0],
+              width,
+              color='#30a2da')
+ah1 = ax21.bar(ind[1], hrtlo_trace_solve_times[0],
+               width)
+ag2 = ax1.bar(ind[0], rtlo_mass_inverse_times[0],
+              width,
+              bottom=rtlo_schur_times[0],
+              color='#fc4f30')
+ah2 = ax21.bar(ind[1], hrtlo_rhs_assembly_times[0],
+               width,
+               bottom=hrtlo_trace_solve_times[0])
+ag3 = ax1.bar(ind[0], rtlo_schur_low_times[0],
+              width,
+              bottom=(rtlo_schur_times[0] +
+                      rtlo_mass_inverse_times[0]),
+              color='#e5ae38')
+ah3 = ax21.bar(ind[1], hrtlo_local_recovery_times[0],
+               width,
+               bottom=(hrtlo_trace_solve_times[0] +
+                       hrtlo_rhs_assembly_times[0]))
+ag4 = ax1.bar(ind[0], rtlo_gmres_other_times[0],
+              width,
+              bottom=(rtlo_schur_times[0] +
+                      rtlo_mass_inverse_times[0] +
+                      rtlo_schur_low_times[0]),
+              color='k')
+ah4 = ax21.bar(ind[1], hrtlo_projection_times[0],
+               width,
+               bottom=(hrtlo_trace_solve_times[0] +
+                       hrtlo_rhs_assembly_times[0] +
+                       hrtlo_local_recovery_times[0]))
+
+ax1.set_yticks(rtlo_time)
+ax21.set_yticks(hrtlo_time)
+for ax in [ax1, ax21]:
+    ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+    plt.setp(ax.get_yminorticklabels(), visible=False)
+    ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter(useOffset=False))
+
+ax1.set_ylabel('gmres time [s] (log)')
+ax21.set_ylabel('hybridization time [s]')
+
+ax1.set_xticklabels(["%s\n%s" % (method[mth], pc) for (mth, pc) in
+                     zip([("RT", 1), ("RT", 1)], ["gmres", "hybridization"])])
+
+ax1.legend((ag1[0], ag2[0], ag3[0], ag4[0]), ("Schur solve",
+                                              "Velocity mass",
+                                              "Apply velocity mass",
+                                              "GMRES other"),
+           bbox_to_anchor=(0.5, 1.5))
+ax21.legend((ah1[0], ah2[0], ah3[0], ah4[0]), ("Trace solve",
+                                               "RHS assembly",
+                                               "Local recovery",
+                                               "Projection"),
+            bbox_to_anchor=(1.25, 1.5))
+
+# BDM2 DG1
+ax22 = ax2.twinx()
+ax2.set_yscale('log')
+# ax22.set_yscale('log')
+
+ax2.bar(ind[0], bdm_schur_times[0],
+        width,
+        color='#30a2da')
+ax22.bar(ind[1], hbdm_trace_solve_times[0],
+         width)
+ax2.bar(ind[0], bdm_mass_inverse_times[0],
+        width,
+        bottom=bdm_schur_times[0],
+        color='#fc4f30')
+ax22.bar(ind[1], hbdm_rhs_assembly_times[0],
+         width,
+         bottom=hbdm_trace_solve_times[0])
+ax2.bar(ind[0], bdm_schur_low_times[0],
+        width,
+        bottom=(bdm_schur_times[0] +
+                bdm_mass_inverse_times[0]),
+        color='#e5ae38')
+ax22.bar(ind[1], hbdm_local_recovery_times[0],
+         width,
+         bottom=(hbdm_trace_solve_times[0] +
+                 hbdm_rhs_assembly_times[0]))
+ax2.bar(ind[0], bdm_gmres_other_times[0],
+        width,
+        bottom=(bdm_schur_times[0] +
+                bdm_mass_inverse_times[0] +
+                bdm_schur_low_times[0]),
+        color='k')
+ax22.bar(ind[1], hbdm_projection_times[0],
+         width,
+         bottom=(hbdm_trace_solve_times[0] +
+                 hbdm_rhs_assembly_times[0] +
+                 hbdm_local_recovery_times[0]))
+
+ax2.set_yticks(bdm_time)
+ax22.set_yticks(hbdm_time)
+for ax in [ax2, ax22]:
+    ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+    plt.setp(ax.get_yminorticklabels(), visible=False)
+    ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter(useOffset=False))
+
+ax2.set_ylabel('gmres time [s] (log)')
+ax22.set_ylabel('hybridization time [s]')
+
+ax2.set_xticklabels(["%s\n%s" % (method[mth], pc) for (mth, pc) in
+                     zip([("BDM", 2), ("BDM", 2)],
+                         ["gmres", "hybridization"])])
+
+
+xlabel = fig.text(0.5, -0.025,
+                  "Discretization",
                   ha='center')
 
-title = fig.text(0.5, 1,
-                 "Approx. Schur Comp. vs Hybrid-mixed methods",
+title = fig.text(0.5, 1.075,
+                 "Approx. Schur-comp. vs hybrid-mixed methods",
                  ha="center")
 
-seaborn.despine(fig)
+fig.subplots_adjust(hspace=0.275)
+# seaborn.despine(fig)
 fig.savefig("williamson_timings.pdf",
             orientation="landscape",
             format="pdf",
