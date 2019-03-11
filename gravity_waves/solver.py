7ba85c5402d79ecc867335c99dd14d299f9ab2d3
Thomas Gibson <t.gibson15@imperial.ac.uk>
2019-03-11 13:25:30 +0000

    Drop unused option

--- gravity_waves/solver.py
+++ gravity_waves/solver.py
@@ -70,8 +70,7 @@ class GravityWaveSolver(object):
                     'pc_gamg_sym_graph': None,
                     'mg_levels': {
                         'ksp_type': 'gmres',
-                        # 'ksp_richardson_scale': 0.55,
-                        'ksp_max_it': 2,
+                        'ksp_max_it': 5,
                         'pc_type': 'bjacobi',
                         'sub_pc_type': 'ilu'
                     }
